ed1b898b5ea953c8e611bf0a89c0f5e343256145
Luka Kraljević <luka.kraljevic90@gmail.com>
2018-05-28 10:05:19 +0200

    model1 finished

---
+++ src/pages/cart.js
@@ -0,0 +1,263 @@
+import React, { Component } from 'react';
+
+import Model1 from '../models/model1';
+import Model2 from '../models/model2';
+import Model3 from '../models/model3';
+import Model4 from '../models/model4';
+
+class Cart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hidden: true,
+            items: new Map(),
+            amount: {
+                total: 0,
+                currency: 'EUR',
+                details: {
+                  subtotal: 0,
+                  tax: 0,
+                  shipping: 0
+                }
+            },
+            discount: {
+                code: '',
+                value: 0,
+                quantity: 0,
+                totalDiscount: 0
+              }
+        }
+
+        this.loadCart();
+    }
+    render() {
+        var className = 'shadow shadow--cart modal'
+        if(this.state.hidden)
+             className += ' modal-hidden';
+        return(
+        <div className={className} id="cart-modal">
+            <div className="container cart">
+            <div className="cart__controls">
+                <a href="#" className="cart__back">Continue Shopping</a>
+                <a href="#" className="cart__close"></a>
+            </div>
+            
+            <div className="card__body">
+                <div className="empty-cart-overlay"></div>
+                <table className="cart__table" id="cart-table">
+                    <CartItems items = {this.state.items} />
+                </table>
+                <div className="cart__buttons">
+                <div className="discount-form">
+                    <div>
+                    <input id="promo-code" name="promocode" className="promo-code" type="text" placeholder="Enter a promo code" autocomplete="off" />
+                    <button className="apply-coupon">Apply</button>
+                    </div>
+                </div>
+                <div>
+                    <button id="cart-sendRequest">Send Request</button>
+                </div>
+                <div>
+                    <div id="paypal-button"></div>
+                </div>
+                </div>
+            </div>
+                <div className="cart_request_form hidden">
+                    <h2 className="cart-form__title">Shooping cart request! Drop us a few lines :)</h2>
+                    <form className="container cart-form" name="cart-form" id="cart-form">
+                        <input type="text" name="name" className="" placeholder="Name:" required="" />
+                        <input type="email" name="email" className="" placeholder="Email:" required="" />
+                        <input type="text" name="subject" className="" placeholder="Subject:" required="" />
+                        <textarea name="message" placeholder="Message:" className="" required=""></textarea>
+                
+                        <div id="recaptcha" className="g-recaptcha" data-sitekey="6LeHJVcUAAAAAMuPh8-LQHuc7cxCP-TCBzEfpUuK" data-callback="cart_onSubmitRequest" data-size="invisible"></div>
+                        <div className="form-buttons">
+                            <button id="card-send-request">
+                                Send Request
+                            </button>
+                            <div id="cart-request-cancel">
+                                Cancel
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>);
+    }
+
+    addItem(model, size, quantity) {
+        var item = {
+          code: model.code,
+          id: this._guid(),
+          name: model.name,
+          quantity: quantity, 
+          size: size,
+          colors: model.currentTheme,
+          price: model.sale_price
+        }
+        
+        var items = this.state.items;
+        items.set(item.id,item);
+
+        this.setState({items}, ()=>{
+            this.updateTotal();
+        });
+    }
+
+    updateTotal() {
+        var subtotal = 0;
+        this.state.items.forEach(function(item){
+          subtotal += item.price.price * Number(item.quantity);
+          subtotal = Number(parseFloat(subtotal).toFixed(2));
+        });
+        
+        this.setState({
+            amount: {
+                details: {subtotal},
+                total: subtotal + this.state.amount.details.tax + this.state.amount.details.shipping
+            }}, ()=>{
+            this.saveCart();
+        })
+
+    }
+     
+
+
+    loadCart() {
+        if (typeof(Storage) !== "undefined") {
+            // Load Items
+            var cart_items = localStorage.getItem('cart_items');
+            if(cart_items) {
+              cart_items = JSON.parse(cart_items);
+              cart_items.forEach((item)=>{
+                this.state.items.set(item.id,item);
+              });
+            }
+            // Load Amount
+            var cart_amount = localStorage.getItem('cart_amount');
+            if(cart_amount)
+                this.state.amount = JSON.parse(cart_amount);
+        }
+    }
+
+    saveCart() {
+        var cart_items = [];
+        this.state.items.forEach(function(item){cart_items.push(item)})
+        localStorage.setItem('cart_items', JSON.stringify(cart_items));
+        localStorage.setItem('cart_amount', JSON.stringify(this.state.amount));
+        console.log("# Cart saved");
+    }
+
+    clearCart() {
+        this.setState({
+            items: new Map(),
+            amount: {
+                total: 0,
+                currency: 'EUR',
+                details: {
+                  subtotal: 0,
+                  tax: 0,
+                  shipping: 0
+                }
+            }
+        })
+
+        this.saveCart();
+    }
+
+    _guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    }
+
+}
+
+
+
+class CartItems extends Component {
+    render() {
+        var items = []
+         this.props.items.forEach((item)=>{
+            items.push(
+                <CartItem id={item.id}
+                code = {item.code}
+                name={item.name} 
+                size={item.size} 
+                total={item.total}
+                quantity = {item.quantity}
+                price = {item.price}/>
+            )
+        });
+        return(
+        <table className="cart__table" id="cart-table">
+            <tr className="cart_header">
+                <th>My Cart ({this.props.items.length})</th>
+                <th>Price</th>
+                <th>Qty</th>
+                <th>Total</th>
+            </tr>
+            {items}
+        </table>
+    )}
+}
+
+class CartItem extends Component {
+    
+    render() {
+        var itemSvg = null;
+        switch(this.props.code) {
+            case 'M1':
+                itemSvg = <Model1/>;
+            break;
+            case 'M2':
+                itemSvg = <Model2/>;
+            break;
+            case 'M3':
+                itemSvg = <Model3/>;
+            break;
+            case 'M4':
+                itemSvg = <Model4/>;
+            break;
+        }
+
+        var size_string = ''
+        if(this.props.size.size_number) size_string += 'Size: '+ this.props.size.size_number + '<br>';
+        if(this.props.size.size1 || this.props.size.size2 || this.props.size.size3 || this.props.size.size4 || this.props.size.size5)
+            size_string += 'Custom size fit: '+ (this.props.size.size1||'-') + ':'+ (this.props.size.size2||'-') + ':'+ (this.props.size.size3||'-') + ':'+ (this.props.size.size4||'-') + ':'+ (this.props.size.size5||'-');
+
+        //apply colors
+        return (
+        <tr className="cart__item">
+            <td>
+                <div className="cart-item__image">
+                    {itemSvg}
+                </div>
+                <div className="cart-item__content">
+                    <div className="cart__model">
+                        {this.props.name}
+                    </div>
+                    <div className="cart__size">
+                        {size_string}
+                    </div>
+                    <a className="cart__remove" href="#" onClick={this.onRemove}>
+                        Remove
+                    </a>
+                </div>
+            </td>
+        <td>{this.props.price.price}€</td>
+        <td>
+            <input type="number" min="1" max="100" value={this.props.quantity}/>
+        </td>
+        <td>{this.props.total}€</td>
+    </tr>
+    )}
+}
+
+
+
+
+export default Cart;
--- src/pages/models.js
+++ src/pages/models.js
@@ -37,7 +37,7 @@ class ModelsPage extends Component {
             break;
         }
         return {activeModel};
-      }
+    }
 }
 export default ModelsPage;
 
@@ -46,60 +46,181 @@ export default ModelsPage;
 class ModelCustomisation extends Component {
     constructor(props) {
         super(props)
-        console.log(props);
+        this.onOrder = this.onOrder.bind(this);
     }
     render() {
         // Applay some logic for custom themese here
-
-        var model = null;
-        switch(this.props.activeModel.code) {
-            case 'M1':
-                model = <Model1 color_theme = {M1Config.themes['default']}/>;
-            break;
-            case 'M2':
-                model = <Model2/>;
-            break;
-            case 'M3':
-                model = <Model3/>;
-            break;
-            case 'M4':
-                model = <Model4/>;
-            break;
-        }
         return (
             <article className="container item">
                 <p className="item__about">Diocletia shoes are handmade leather shoes with a „dash of history“. Playing with the colors and traditional style, these Diocletia's shoes are designed to fulfill the demands of contemporary woman and her lifestyle.</p>
-                <div className="item__model" id="model-info">
-                    <h3>Model</h3>
-                    <div id="item-price" className="item__price promo">
-                        <del>
-                            <span class="amount">&infin;€</span>
-                        </del>
-                        <ins>
-                            <span class="amount">&infin;€</span>
-                        </ins>
-                    </div>
-                </div>
-                <div className="item__view" id="model">
-                    {model}
-                    <div className="item__colors" id="colors">
-                        <a className="item__color" data-color="red" style={{backgroundColor: 'red'}} href="#"></a>
-                        <a className="item__color" data-color="green_light" style={{backgroundColor: '#1aa51a'}} href="#"></a>
-                        <a className="item__color" data-color="crimson" style={{backgroundColor: '#af1111'}} href="#"></a>
-                        <a className="item__color" data-color="yellow" style={{backgroundColor: '#eadc6d'}} href="#"></a>
-                        <a className="item__color" data-color="cyclamen" style={{backgroundColor: '#ce3a77'}} href="#"></a>
-                        <a className="item__color" data-color="green_dark" style={{backgroundColor: '#156915'}} href="#"></a>
-                        <a className="item__color" data-color="purple" style={{backgroundColor: '#840484'}} href="#"></a>
-                        <a className="item__color" data-color="orange" style={{backgroundColor: '#fd9914'}} href="#"></a>
-                    </div>
-                </div>
-                <SizeForm />
-              </article>
+                <ModelInfo />
+                <ModelView {...this.props}/>
+                <SizeForm  onOrder={this.onOrder}/>
+            </article>
         )}
 }
 
+class ModelView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            _activeColor: null,
+            _activeStraps : null,
+            code: null,
+            size: null,
+            style: null,
+        }
+
+        // Callbacks
+        this.activateColor = this.activateColor.bind(this);
+        this.updateStraps = this.updateStraps.bind(this);
+
+        //this.updateColorStyle = this.updateColorStyle.bind(this);
+    }
+
+    render() {
+
+        var Model = null;
+        switch(this.props.activeModel.code) {
+            case 'M1': {
+                
+                Model = <Model1  
+                activeStraps = {this.state._activeStraps}
+                colorStyle = {this.state.colorStyle} 
+                onStrapSelection = {this.updateStraps} />;
+            }
+            break;
+            case 'M2':
+                Model = <Model2/>;
+            break;
+            case 'M3':
+                Model = <Model3/>;
+            break;
+            case 'M4':
+                Model = <Model4/>;
+            break;
+        }
+        return(
+            <div className="item__view" id="model">
+                    {Model}
+                    <ColorPalete 
+                    activeColor={this.state._activeColor} 
+                    onColorActivate={this.activateColor}/>
+                </div>
+        )
+    }
+
+    activateColor(color) {
+        
+        this.setState((prevState, props)=> {
+            if(prevState._activeColor == color)
+                return {_activeColor: null}
+            else
+                return {_activeColor: color}
+        }, () => {
+            this.updateColorStyle()
+        })
+    }
+
+    updateStraps(strObj) {
+        this.setState((prevState, props)=>{
+
+            if ('l' in strObj) 
+                prevState._activeStraps.l[strObj.l] = !prevState._activeStraps.l[strObj.l]
+
+            if ('r' in strObj) 
+                prevState._activeStraps.r[strObj.r] = !prevState._activeStraps.r[strObj.r]
 
 
+            return {_activeStraps: prevState._activeStraps}
+        }, () => {
+            this.updateColorStyle()
+        })
+    }
+
+    updateColorStyle() {
+        console.log(this.state);
+        function _isempty(array) {
+            return array.indexOf(true) != -1
+        }
+        this.setState((prevState, props)=>{
+            
+            if(prevState._activeStraps && (_isempty(prevState._activeStraps.l )|| _isempty(prevState._activeStraps.r)))  
+                if(prevState._activeColor){
+
+                    prevState._activeStraps.l.forEach((strap, i)=>{
+                        if(strap) prevState.colorStyle.l[i] =  prevState._activeColor;
+                    });
+                    prevState._activeStraps.r.forEach((strap, i)=>{
+                        if(strap) prevState.colorStyle.r[i] =  prevState._activeColor;
+                    });
+                    return {
+                    _activeColor: null,
+                    _activeStraps: {
+                        l : prevState._activeStraps.l.map(()=>{false}),
+                        r : prevState._activeStraps.r.map(()=>{false})
+                    },
+                    colorStyle: prevState.colorStyle
+                    }
+                }
+        });
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        var code, colorStyle, _activeStraps;
+        switch(nextProps.activeModel.code) {
+            case 'M1':
+                code = 'M1';
+                colorStyle = M1Config.themes['default'];
+                _activeStraps = {
+                    l: Array(M1Config.nr_strips).map(()=>{false}),
+                    r: Array(M1Config.nr_strips).map(()=>{false})
+                };
+            break;
+            case 'M2':
+                code = 'M2';
+                colorStyle = M2Config.themes['default'];
+            break;
+            case 'M3':
+                code = 'M3';
+                colorStyle = M3Config.themes['default'];
+            break;
+            case 'M4':
+                code = 'M4';
+                colorStyle = M4Config.themes['default'];
+            break;
+        }
+        return {code, colorStyle, _activeStraps};
+    }
+}
+
+
+class ColorPalete extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return(
+            <div className="item__colors" id="colors">
+                <a className={"item__color"+((this.props.activeColor=="r1")?" active":"")} data-color="red" style={{backgroundColor: 'red'}} href="#" onClick={this.activateColor.bind(this,"r1")}></a>
+                <a className={"item__color"+((this.props.activeColor=="g1")?" active":"")} data-color="green_light" style={{backgroundColor: '#1aa51a'}} href="#" onClick={this.activateColor.bind(this,"g1")}></a>
+                <a className={"item__color"+((this.props.activeColor=="c1")?" active":"")} data-color="crimson" style={{backgroundColor: '#af1111'}} href="#" onClick={this.activateColor.bind(this,"c1")}></a>
+                <a className={"item__color"+((this.props.activeColor=="y1")?" active":"")} data-color="yellow" style={{backgroundColor: '#eadc6d'}} href="#" onClick={this.activateColor.bind(this,"y1")}></a>
+                <a className={"item__color"+((this.props.activeColor=="c2")?" active":"")} data-color="cyclamen" style={{backgroundColor: '#ce3a77'}} href="#" onClick={this.activateColor.bind(this,"c2")}></a>
+                <a className={"item__color"+((this.props.activeColor=="g2")?" active":"")} data-color="green_dark" style={{backgroundColor: '#156915'}} href="#" onClick={this.activateColor.bind(this,"g2")}></a>
+                <a className={"item__color"+((this.props.activeColor=="p1")?" active":"")} data-color="purple" style={{backgroundColor: '#840484'}} href="#" onClick={this.activateColor.bind(this,"p1")}></a>
+                <a className={"item__color"+((this.props.activeColor=="o1")?" active":"")} data-color="orange" style={{backgroundColor: '#fd9914'}} href="#" onClick={this.activateColor.bind(this,"o1")}></a>
+            </div>
+        )
+    }
+
+    activateColor(color, e) {
+        e.preventDefault();
+        this.props.onColorActivate(color)
+    }
+
+}
 
 
 class SizeForm extends Component {
@@ -112,10 +233,10 @@ class SizeForm extends Component {
                 <input type="text" id="size2" name="s2" placeholder="2 - Ball circumference" />
                 <input type="text" id="size3" name="s3" placeholder="3 - Instep circumference" />
                 <input type="text" id="size4" name="s4" placeholder="4 - Heel girth" />
-                <input type="text" id="size5" name="s5" placeholder="5 - Ankle girth" />
-                <p className="link-sizeguide item-form__about size-guide-link" style={{textDecoration: 'underline', cursor:'pointer'}}>
+                <input type="text" id="size5" name="s5" placeholder="5 - Ankle girth" />    
+            </div>
+            <p className="link-sizeguide item-form__about size-guide-link" style={{textDecoration: 'underline', cursor:'pointer'}}>
                 Size guide instructions for each length and girth measurments.</p>
-             </div>
             <div className="item__dropdown">
                 <h4 className="item-form__title">or choose shoe size</h4>
                 <select id="size_number" name="size_number">
@@ -127,12 +248,30 @@ class SizeForm extends Component {
                     <option value="40">40</option>
                     <option value="41">41</option>
                 </select>
-                <button class="button--gold" id="add_cart">Order Now</button>
+                <button class="button--gold" id="add_cart" onClick={this.props.orderNow.bind(this)}>Order Now</button>
             </div>
         </div>
         )
     }
+    orderNow() {
+        // Validation
+        this.props.onOrder();
+    }
 }
+
+const ModelInfo = (props={})=>(
+    <div className="item__model" id="model-info">
+                    <h3>Model</h3>
+                    <div id="item-price" className="item__price promo">
+                        <del>
+                            <span class="amount">&infin;€</span>
+                        </del>
+                        <ins>
+                            <span class="amount">&infin;€</span>
+                        </ins>
+                    </div>
+                </div>
+)
 
 
 
