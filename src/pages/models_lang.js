ee83388e86510321ab20c43b65edd6891890032a
Luka Kraljević <luka.kraljevic90@gmail.com>
2018-06-14 11:26:21 +0200

    commit

---
+++ src/pages/models_lang.js
@@ -0,0 +1,11 @@
+const hr_HR = {
+    itemAbout: ""   
+}
+
+
+
+
+
+const en_US = {
+    itemAbout: "Diocletia shoes are handmade leather shoes with a „dash of history“. Playing with the colors and traditional style, these Diocletia's shoes are designed to fulfill the demands of contemporary woman and her lifestyle."   
+}
